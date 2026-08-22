b651d7be78c85fc0a931e14dfb9eebe905243a82
Quiche <quiche.inquiries@gmail.com>
2026-08-21 21:50:26 -0400

    Added basic dotenv setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response } from 'express';
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+// load environment vars
+const deployEnv = process.env.NODE_ENV || "dev";
+const envFile = deployEnv === "prod" ? ".env.production" : ".env.development";
+dotenv.config({ path: envFile });
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response) => {
-  res.status(200).json({ status: 'ok' });
+app.get("/status", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
 });
 
 app.listen(3000, () => {
-  console.log('Server listening at http://localhost:3000');
+  console.log("Server listening at http://localhost:3000");
+  console.log(`Target environment: ${deployEnv}`);
 });
