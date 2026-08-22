91e6ff0fb9e883d23231dcc77572bbffe1290054
Keith Zhang <keithohno@gmail.com>
2026-08-22 16:45:58 -0400

    Connected frontend registration flow to backend server

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,6 +1,7 @@
-import { User } from '../types';
+import { User } from "../types";
 
-const MOCK_USER: User = { id: 'u3', name: 'You', email: 'you@example.com' };
+const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL;
+const MOCK_USER: User = { id: "u3", name: "You", email: "you@example.com" };
 
 function fakeDelay(ms = 300): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -9,17 +10,37 @@ function fakeDelay(ms = 300): Promise<void> {
 export async function login(email: string, password: string): Promise<User> {
   await fakeDelay();
   if (email && password) return MOCK_USER;
-  throw new Error('Invalid credentials');
+  throw new Error("Invalid credentials");
   // Later: return fetch(`${API_URL}/auth/login`, {
   //   method: 'POST',
   //   body: JSON.stringify({ email, password }),
   // }).then((r) => r.json());
 }
 
-export async function signup(name: string, email: string, password: string): Promise<User> {
-  await fakeDelay();
-  if (name && email && password) return { ...MOCK_USER, name, email };
-  throw new Error('Missing signup fields');
+export async function signup(
+  name: string,
+  email: string,
+  password: string,
+): Promise<User> {
+  console.log(`Sending request for user registration: ${name}`);
+  const res = await fetch(`http://${SERVER_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: name,
+      email,
+      password,
+    }),
+  });
+
+  const data = await res.json();
+  if (data.error) throw new Error(data.error.message);
+  else {
+    console.log(`Registered user: ${name}`);
+    return { id: data.id, name: data.username, email: data.email };
+  }
 }
 
 export async function logout(): Promise<boolean> {
