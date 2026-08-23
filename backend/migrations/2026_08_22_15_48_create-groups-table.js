eeab8e81c49fc5d75797c51853be6c793ccd0094
unknown <jackliu09282001@gmail.com>
2026-08-23 12:31:03 -0400

    testedgroups and auth still works so w

--- backend/migrations/2026_08_22_15_48_create-groups-table.js
+++ backend/migrations/2026_08_22_15_48_create-groups-table.js
@@ -4,11 +4,11 @@
 exports.up = (pgm) => {
   pgm.createTable("groups", {
     id: { type: "uuid", primaryKey: true },
-    name: { type: "text", notNull: true, unique: true },
-    ownerId: { type: "text", notNull: true, unique: true },
-    inviteCode: { type: "text", notNull: true},
-    memberCount: { type: "integer", notNull: true, default: 0},
-    seasonLength: { type: "integer", notNull: true, deafult: 30},
+    name: { type: "text", notNull: true },
+    owner_id: { type: "text", notNull: true },
+    invite_code: { type: "text", notNull: true},
+    member_count: { type: "integer", notNull: true, default: 1},
+    season_length: { type: "integer", notNull: true, deafult: 30},
     created_at: {
       type: "timestamptz",
       notNull: true,
