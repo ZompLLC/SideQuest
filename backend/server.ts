262dc6b1f44f096ce00b35884c4d639817235212
Keith Zhang <keithohno@gmail.com>
2026-08-23 16:53:11 -0400

    requireAuth middleware should not be globally applied

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,6 @@ import { logger, requestLogger, winstonLogger } from "./src/logger.js";
 import { authRouter } from "./src/routes/auth.routes.js";
 import { groupRouter } from "./src/routes/group.routes.js";
 import { userRouter } from "./src/routes/user.routes.js";
-import { requireAuth } from "./src/middleware/requireAuth.js";
 
 // load environment vars
 const deployEnv = process.env.NODE_ENV || "dev";
@@ -14,7 +13,6 @@ const app = express();
 
 app.use(express.json());
 app.use(logger);
-app.use(requireAuth);
 app.use(requestLogger);
 
 app.get("/status", (req: Request, res: Response) => {
